d192a77a2a8a41a2ca3beeb85ad10c7c9c3af17c
AbinashG <abhi24964@gmail.com>
2021-07-12 17:46:50 +0530

    Completed course

--- courses/views.py
+++ courses/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Course
 
 def index(request):
-    return HttpResponse('Hello there!')
+    # return HttpResponse('Hello there!')
+    courses = Course.objects
 
+    return render(request, 'courses/index.html', {'courses':courses})
